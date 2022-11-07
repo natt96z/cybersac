ff7da744603a975a6a90c583cc61bcaf2ecaa82f
Nathaniel Clay <112590005+natt96z@users.noreply.github.com>
2022-11-07 00:39:54 -0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,16 +3,8 @@
 # -- Project information
 
 project = 'Documentation Hub Ver: 1.9.0'
-copyright = '2022, Santa Ana College/Nathaniel Clay'
-author = ' CyberPatriot Authors/Contributors Include:
-   
-   *Nathaniel Clay* - Programming Software and hardware, diagnostics and repair.
-   
-   *Teairra McCoy* - Multi-operating system support and STEM Technology.
-   
-   *Albert Huynh* - Software security hardening and OS security diagnostics.
-   
-   *Hung Dao* - Programming Online Software and Company Analystic.'
+copyright = '2022, Santa Ana College & Nathaniel Clay'
+author = ' CyberPatriot Authors/Contributors Include Nathaniel Clay, Teairra McCoy, Albert Huyh, Hung Dao
 
 release = '0.1'
 version = '0.1.0'
