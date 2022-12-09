a45686c49a887b5402ad41c8446a2bbd766ce432
Nathaniel Clay <112590005+natt96z@users.noreply.github.com>
2022-12-09 01:58:06 -0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,8 +30,13 @@ templates_path = ['_templates']
 # -- Options for HTML output
 
 html_theme = 'sphinx_rtd_theme'
+html_logo = '_img/logo.png'
+html_theme_options = {
+    "logo_only": True,
+    "display_version": False,
+}
+
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
 
-html_logo = '_img/logo.png'
