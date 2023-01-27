b534a5bc468bc933990369953e2ca0170740113d
Nathaniel Clay <112590005+natt96z@users.noreply.github.com>
2023-01-26 21:39:48 -0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,12 +2,12 @@
 
 # -- Project information
 
-project = 'Documentation Hub Ver: 2.5.1'
+project = 'Documentation Hub Ver: 2.5.3'
 copyright = '2023, Santa Ana College & CyberPatriot Authors/Contributors'
 author = 'Nathaniel Clay'
 
 release = '2.5'
-version = '2.5.1'
+version = '2.5.3'
 
 # -- General configuration
 
